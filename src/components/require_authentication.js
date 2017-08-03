7d84942d502f74b14a02a7ebd226efe8b1186c85
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-08-03 14:00:39 +0700

    Hook Up HoC with context

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -3,8 +3,33 @@ import { connect } from 'react-redux';
 
 export default function (ComposedComponent) {
   class Authentication extends Component {
+    
+    // This is required so our component can use this.context
+    // We pass properties from router (react-router) to this component
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    // When the component is about to be rendered,
+    // if the user is not authenticated, then redirect them to home
+    componentWillMount(){
+      if (!this.props.authenticated){
+        this.context.router.history.push('/');
+      }
+    }
+
+    // When the component is going to change state (from authenticated (singed in) to not authenticated (signed out) )
+    // redirect the user to home
+    componentWillUpdate(nextProps){
+      if (!nextProps.authenticated){
+        this.context.router.history.push('/');
+      }
+
+    }
+
+    
     render() {
-      console.log(this.props.authenticated);
+      
       return <ComposedComponent {...this.props} />;
     }
   }
